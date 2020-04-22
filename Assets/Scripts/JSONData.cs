ca0bdc1e4033ff66dad56abee2b43526dca9fb67
kpatkiRIT <kp9659@g.rit.edu>
2020-04-22 18:18:16 -0400

    Added sample json reading, sample prefab instantiation and destruction

---
+++ Assets/Scripts/JSONData.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JSONData : MonoBehaviour
+{
+	string filename = "data.json";
+	string path;
+
+	GameData gamedata = new GameData();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	path = Application.persistentDataPath + "/" + filename;
+    	Debug.Log( path );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if( Input.GetKey( "up" ) )
+    	{
+    		ReadData();
+    	}   
+    }
+
+    void ReadData()
+    {
+    	string contents = System.IO.File.ReadAllText( path );
+    	gamedata = JsonUtility.FromJson<GameData>( contents );
+    	// gamedata.firstname = "bla";
+    	// gamedata.lastname = "bulla";
+    	Debug.Log(gamedata.firstname);
+    	Debug.Log(gamedata.lastname);
+    }
+}
